87b1da96d300542741b722d3f8a1fd66d32bc1c9
aks-builds <its.aks@outlook.com>
2026-06-19 13:15:00 +0530

    chore: enforce double quotes via eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,5 +4,6 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
+    quotes: ["warn", "double", { avoidEscape: true }],
   },
 });
